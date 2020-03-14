2777b9677c5d3c94de0b88822f80b12860c652b7
akshayakky <akshaysmpt01@gmail.com>
2020-03-14 17:54:43 +0530

    ADDED : Test Case TC1.1, TC1.2, TC1.3

--- src/test/java/FindMaximumTestCases.java
+++ src/test/java/FindMaximumTestCases.java
@@ -8,4 +8,18 @@ public class FindMaximumTestCases {
         Integer max = findMaximum.findMaximumInteger(3, 2, 1);
         Assert.assertEquals((Integer) 3, max);
     }
+
+    @Test
+    public void givenThreeIntegers_WhenMaximumFoundAtPosition2_ThenReturnMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer max = findMaximum.findMaximumInteger(2, 3, 1);
+        Assert.assertEquals((Integer) 3, max);
+    }
+
+    @Test
+    public void givenThreeIntegers_WhenMaximumFoundAtPosition3_ThenReturnMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer max = findMaximum.findMaximumInteger(1, 2, 3);
+        Assert.assertEquals((Integer) 3, max);
+    }
 }
