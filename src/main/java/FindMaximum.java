bed97f8c2da804d6b60ea0096c0cdcaea4250e70
akshayakky <akshaysmpt01@gmail.com>
2020-03-14 17:51:30 +0530

    ADDED : Function To Find Maximum Integer Value

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -2,4 +2,13 @@ public class FindMaximum {
     public static void main(String[] args) {
         System.out.println("Welcome To Find Maximum Problem Using Generics");
     }
+
+    public static Integer findMaximumInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        Integer maximumValue = firstValue;
+        if (secondValue.compareTo(maximumValue) > 0)
+            maximumValue = secondValue;
+        if (thirdValue.compareTo(maximumValue) > 0)
+            maximumValue = thirdValue;
+        return maximumValue;
+    }
 }
--- src/test/java/FindMaximumTestCases.java
+++ src/test/java/FindMaximumTestCases.java
@@ -1,2 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FindMaximumTestCases {
+    @Test
+    public void givenThreeIntegers_WhenMaximumFoundAtPosition1_ThenReturnMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer max = findMaximum.findMaximumInteger(3, 2, 1);
+        Assert.assertEquals((Integer) 3, max);
+    }
 }
