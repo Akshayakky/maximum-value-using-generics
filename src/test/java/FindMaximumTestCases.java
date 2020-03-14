210d5d97b5e5b869a33c48e639886e3429b72503
akshayakky <akshaysmpt01@gmail.com>
2020-03-14 18:03:01 +0530

    ADDED : Test Cases TC2.1, TC2.2, TC2.3

--- src/test/java/FindMaximumTestCases.java
+++ src/test/java/FindMaximumTestCases.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FindMaximumTestCases {
+    //Test Cases For Integer Values
     @Test
     public void givenThreeIntegers_WhenMaximumFoundAtPosition1_ThenReturnMaximum() {
         FindMaximum findMaximum = new FindMaximum();
@@ -21,5 +22,27 @@ public class FindMaximumTestCases {
         FindMaximum findMaximum = new FindMaximum();
         Integer max = findMaximum.findMaximumInteger(1, 2, 3);
         Assert.assertEquals((Integer) 3, max);
+    }
+
+    //Test Cases For Float Values
+    @Test
+    public void givenThreeFloats_WhenMaximumFoundAtPosition1_ThenReturnMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Float max = findMaximum.findMaximumFloat(3.1f, 2.4f, 1.3f);
+        Assert.assertEquals((Float) 3.1f, max);
+    }
+
+    @Test
+    public void givenThreeFloats_WhenMaximumFoundAtPosition2_ThenReturnMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Float max = findMaximum.findMaximumFloat(2.4f, 3.1f, 1.3f);
+        Assert.assertEquals((Float) 3.1f, max);
+    }
+
+    @Test
+    public void givenThreeFloats_WhenMaximumFoundAtPosition3_ThenReturnMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Float max = findMaximum.findMaximumFloat(1.3f, 2.4f, 3.1f);
+        Assert.assertEquals((Float) 3.1f, max);
     }
 }
